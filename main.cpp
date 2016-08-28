0d4b5c34dcf8e7ba167c0e2d08a552c854476811
Sir Nightmare <ivantochilkin@gmail.com>
2016-08-28 18:49:48 +0300

    everything is done

--- main.cpp
+++ main.cpp
@@ -2,14 +2,25 @@
 
 using namespace std;
 
+inline int triNum(long long input) {
+    int n = 1;
+    int triangleNumber = 0;
+    while (triangleNumber < input) {
+        triangleNumber = n * (n + 1) / 2;
+        if (triangleNumber == input) {
+            return n;
+        }
+        n++;
+    }
+    return 0;
+}
+
 int foo() {
     long long input = 0;
     int is_double = 0;
-    if (scanf("%llu.%d", &input, &is_double) != 1) {
-        if (is_double != 0) {
-            while (true) {
-
-            }
+    if (scanf("%llu.%d", &input, &is_double) == 1) {
+        if (input > 0) {
+            return triNum(input);
         }
     }
     return 0;
